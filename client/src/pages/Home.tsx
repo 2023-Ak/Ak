77c06bdaa3dcfdf22cf7901d815d68a9bba0c0dd
MoYuBin <bin8727@naver.com>
2023-07-19 13:21:34 +0900

    Main Page Design

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { styled } from "styled-components";
 import Main from "../components/main-section/Main";
+import LinkedPage from "../components/main-section/LinkedPage";
 
 const Home = () => {
   return (
     <Container>
       <Main />
+      <LinkedPage />
     </Container>
   );
 };
@@ -13,8 +15,10 @@ export default Home;
 
 const Container = styled.div`
   display: flex;
+  height: 66.5625rem;
   padding: 8rem 0rem;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  gap: 6.25rem;
   align-self: stretch;
 `;
